bd05f56e98b2405432551de203a3cfc31f5abd70
Kashf Noor <noor492k@gmail.com>
2025-02-27 14:26:51 +0500

    Update deque3&4.cpp

--- Week4/deque3&4.cpp
+++ Week4/deque3&4.cpp
@@ -1,26 +1,26 @@
-//
-//#include "deque3&4.h"
-//
-//int main() {
-//    string str;
-//    cout << "Enter your string to find out whether its a palindrome or not \n";
-//    getline(cin, str);
-//    Deque<char> character(str.length());
-//
-//    for (char ch : str) {
-//        character.insertRear(ch);
-//    }
-//    char removedFront, removedRear;
-//
-//    for (int i = 0; i < str.length() / 2; i++) {
-//
-//        character.removeFront(removedFront);
-//        character.removeRear(removedRear);
-//
-//        if (removedFront != removedRear) {
-//            cout << "Your string is not a palindrome :(";
-//            return 0;
-//        }
-//    }
-//    cout << "Your string is a palindrome :)";
-//}
+
+#include "deque3&4.h"
+
+int main() {
+    string str;
+    cout << "Enter your string to find out whether its a palindrome or not \n";
+    getline(cin, str);
+    Deque<char> character(str.length());
+
+    for (char ch : str) {
+        character.insertRear(ch);
+    }
+    char removedFront, removedRear;
+
+    for (int i = 0; i < str.length() / 2; i++) {
+
+        character.removeFront(removedFront);
+        character.removeRear(removedRear);
+
+        if (removedFront != removedRear) {
+            cout << "Your string is not a palindrome :(";
+            return 0;
+        }
+    }
+    cout << "Your string is a palindrome :)";
+}
