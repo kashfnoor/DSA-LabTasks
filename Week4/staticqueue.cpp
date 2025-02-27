64fcb331ba74566f47af5b95ab08234af37419bf
Kashf Noor <noor492k@gmail.com>
2025-02-27 14:28:12 +0500

    Update staticqueue.cpp

--- Week4/staticqueue.cpp
+++ Week4/staticqueue.cpp
@@ -1,17 +1,31 @@
-// (4.1 static version)
-//#include "staticqueue.h"
-//int main()
-//{
-//    Queue<int> numbers;
-//    cout << "Entering ";
-//    for (int i = 1; i <= 10; i++) {
-//        numbers.insert(i);
-//        cout << i << " ";
-//    }
-//    cout << "\Removal ";
-//    int num;
-//    for (int i = 0; i < 10; i++) {
-//        numbers.remove(num);
-//        cout << num << " ";
-//    }
-//}
+#include "dynamicqueue.h"
+int main() {
+    Queue<int> numbers(10);
+    cout << "Entering ";
+    for (int i = 0; i < 10; i++) {
+        numbers.insert(i);
+        cout << i << " ";
+    }
+    int num;
+    cout << "\nRemoval ";
+    for (int i = 0; i < 10; i++) {
+        numbers.remove(num);
+        cout << num << " ";
+    }
+    Queue<string> names(8);
+    cout << "\n\nNames of Friends : \n";
+    names.insert(" Khadija");
+    names.insert("Habibti");
+    names.insert("Hamza");
+    names.insert("Zainy");
+    names.insert("Rohan");
+    names.insert("Mary");
+    names.insert("Sabo");
+    names.insert("Fatima");
+    string friends;
+    for (int i = 0; i < 8; i++) {
+        names.remove(friends);
+        cout << friends << "\n ";
+    }
+    cout << "\n\n";
+}
